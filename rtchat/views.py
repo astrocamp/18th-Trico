53fcde8345ffaaf1a1cd100059d5bf93c4a7e6f8
Michelle Lai <ktlaiktlai@gmail.com>
2025-01-09 12:26:54 +0800

    fix: real-time chat function

--- rtchat/views.py
+++ rtchat/views.py
@@ -6,88 +6,164 @@ from .forms import ChatmessageCreateForm
 import uuid  
 from django.contrib.auth.models import User
 
-
 @login_required
-def chat_view(request, chatroom_name):
-    # 查找對應的聊天房間
+def chat_view(request, chatroom_name='public-chat'):
     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
-
-    # 驗證聊天室是否為私人房間，以及成員是否只有兩人
-    if not chat_group.is_private or chat_group.members.count() != 2:
-        raise Http404("聊天室不存在或無法訪問")
-
-    # 確認當前用戶是否為該聊天室成員
-    if request.user not in chat_group.members.all():
-        raise Http404("您無法訪問此聊天室")
-
-    # 找出聊天對象
-    other_user = None
-    for member in chat_group.members.all():
-        if member != request.user:
-            other_user = member
-            break
-
-    # 取得最近的聊天消息
     chat_messages = chat_group.chat_messages.all()[:30]
-
-    # 聊天訊息表單
     form = ChatmessageCreateForm()
-
     
-
-    # HTMX 請求處理
+    other_user = None
+    if chat_group.is_private:
+        if request.user not in chat_group.members.all():
+            raise Http404()
+        for member in chat_group.members.all():
+            if member != request.user:
+                other_user = member
+                break
+            
+#     if chat_group.groupchat_name:
+#         if request.user not in chat_group.members.all():
+#             if request.user.emailaddress_set.filter(verified=True).exists():
+#                 chat_group.members.add(request.user)
+#             else:
+#                 messages.warning(request, 'You need to verify your email to join the chat!')
+#                 return redirect('profile-settings')
+    
     if request.htmx:
         form = ChatmessageCreateForm(request.POST)
-        if form.is_valid():
+        if form.is_valid:
             message = form.save(commit=False)
             message.author = request.user
             message.group = chat_group
             message.save()
             context = {
-                "message": message,
-                "user": request.user,
+                'message' : message,
+                'user' : request.user
             }
-            return render(request, "rtchat/partials/chat_message_p.html", context)
-
+            return render(request, 'a_rtchat/partials/chat_message_p.html', context)
+    
     context = {
-        "chat_messages": chat_messages,
-        "form": form,
-        "other_user": other_user,
-        "chatroom_name": chatroom_name,
+        'chat_messages' : chat_messages, 
+        'form' : form,
+        'other_user' : other_user,
+        'chatroom_name' : chatroom_name,
+        'chat_group' : chat_group
     }
-    return render(request, "rtchat/chat.html", context)
-
-
-
-
+    
+    return render(request, 'a_rtchat/chat.html', context)
 
 @login_required
 def get_or_create_chatroom(request, username):
-    # 禁止用戶與自己聊天
     if request.user.username == username:
-        return redirect('services:service_detail')
-
-    # 根據 username 獲取其他用戶
-    other_user = get_object_or_404(User, username=username)
-
-    # 查找是否已經有私聊房間
+        return redirect('users:profile_by_username')
+    
+    other_user = User.objects.get(username = username)
     my_chatrooms = request.user.chat_groups.filter(is_private=True)
-
-    chatroom = None
+    
+    
     if my_chatrooms.exists():
         for chatroom in my_chatrooms:
             if other_user in chatroom.members.all():
+                chatroom = chatroom
                 break
-        else:
-            # 如果沒有找到現有的房間，創建新的
-            chatroom_name = str(uuid.uuid4())  # 生成唯一房間名稱
-            chatroom = ChatGroup.objects.create(group_name=chatroom_name, is_private=True)
-            chatroom.members.add(request.user, other_user)
+            else:
+                chatroom = ChatGroup.objects.create(is_private = True)
+                chatroom.members.add(other_user, request.user)
     else:
-        # 如果用戶沒有任何房間，創建新的
-        chatroom_name = str(uuid.uuid4())
-        chatroom = ChatGroup.objects.create(group_name=chatroom_name, is_private=True)
-        chatroom.members.add(request.user, other_user)
+        chatroom = ChatGroup.objects.create(is_private = True)
+        chatroom.members.add(other_user, request.user)
+        
+    return redirect('chatroom', chatroom.group_name)
 
-    # 重定向到聊天房間
-    return redirect('chatroom', chatroom_name=chatroom.group_name)
+
+# @login_required
+# def create_groupchat(request):
+#     form = NewGroupForm()
+    
+#     if request.method == 'POST':
+#         form = NewGroupForm(request.POST)
+#         if form.is_valid():
+#             new_groupchat = form.save(commit=False)
+#             new_groupchat.admin = request.user
+#             new_groupchat.save()
+#             new_groupchat.members.add(request.user)
+#             return redirect('chatroom', new_groupchat.group_name)
+    
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'a_rtchat/create_groupchat.html', context)
+
+
+# @login_required
+# def chatroom_edit_view(request, chatroom_name):
+#     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+#     if request.user != chat_group.admin:
+#         raise Http404()
+    
+#     form = ChatRoomEditForm(instance=chat_group) 
+    
+#     if request.method == 'POST':
+#         form = ChatRoomEditForm(request.POST, instance=chat_group)
+#         if form.is_valid():
+#             form.save()
+            
+#             remove_members = request.POST.getlist('remove_members')
+#             for member_id in remove_members:
+#                 member = User.objects.get(id=member_id)
+#                 chat_group.members.remove(member)  
+                
+#             return redirect('chatroom', chatroom_name) 
+    
+#     context = {
+#         'form' : form,
+#         'chat_group' : chat_group
+#     }   
+#     return render(request, 'a_rtchat/chatroom_edit.html', context) 
+
+
+# @login_required
+# def chatroom_delete_view(request, chatroom_name):
+#     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+#     if request.user != chat_group.admin:
+#         raise Http404()
+    
+#     if request.method == "POST":
+#         chat_group.delete()
+#         messages.success(request, 'Chatroom deleted')
+#         return redirect('home')
+    
+#     return render(request, 'a_rtchat/chatroom_delete.html', {'chat_group':chat_group})
+
+
+# @login_required
+# def chatroom_leave_view(request, chatroom_name):
+#     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+#     if request.user not in chat_group.members.all():
+#         raise Http404()
+    
+#     if request.method == "POST":
+#         chat_group.members.remove(request.user)
+#         messages.success(request, 'You left the Chat')
+#         return redirect('home')
+    
+    
+# def chat_file_upload(request, chatroom_name):
+#     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    
+#     if request.htmx and request.FILES:
+#         file = request.FILES['file']
+#         message = GroupMessage.objects.create(
+#             file = file,
+#             author = request.user, 
+#             group = chat_group,
+#         )
+#         channel_layer = get_channel_layer()
+#         event = {
+#             'type': 'message_handler',
+#             'message_id': message.id,
+#         }
+#         async_to_sync(channel_layer.group_send)(
+#             chatroom_name, event
+#         )
+#     return HttpResponse()
